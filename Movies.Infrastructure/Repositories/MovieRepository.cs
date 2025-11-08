38bb7dfbe31d462790a8e962dddbbdbe241943d0
Mouhamed Aminata Madieng NDOUR <mouhamed.ndour@younited-credit.fr>
2025-11-08 17:06:06 +0100

    Add generic and movie-specific repository implementations
    Introduce a generic repository in `Repository.cs` to manage
    entities with common CRUD operations using Entity Framework
    Core. Add a `MovieRepository` in `MovieRepository.cs` for
    movie-specific operations, including a method to fetch movies
    by director name. These changes follow a clean architecture
    pattern, improving code reusability and maintainability.

---
+++ Movies.Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Core.Entities;
+using Movies.Core.Repositories;
+using Movies.Infrastructure.Data;
+using Movies.Infrastructure.Repositories.Base;
+
+namespace Movies.Infrastructure.Repositories
+{
+    public class MovieRepository : Repository<Movie>, IMovieRepository
+    {
+
+
+        public MovieRepository(MovieContext movieContext) : base(movieContext)
+        {
+
+        }
+
+        public async Task<IReadOnlyList<Movie>> GetMoviesByDirectorNameAsync(string directorName)
+        {
+            return await _movieContext.Movies
+                .Where(m => m.DirectorName == directorName)
+                .ToListAsync();
+        }
+    }
+}
