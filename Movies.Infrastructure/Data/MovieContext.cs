9800f7848b95a98a819e8e4fc745e46d52691c2c
Mouhamed Aminata Madieng NDOUR <mouhamed.ndour@younited-credit.fr>
2025-11-08 16:50:25 +0100

    Add EF Core context and seeding for Movie entities
    Introduced `MovieContext` as the Entity Framework Core database context for managing `Movie` entities. Added `MovieContextSeed` to seed the database with initial data, including retry logic and logging. Updated `Movies.Infrastructure.csproj` to include necessary EF Core packages. Removed the unused `Class1` class to clean up the codebase.

---
+++ Movies.Infrastructure/Data/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Core.Entities;
+
+namespace Movies.Infrastructure.Data
+{
+    public class MovieContext: DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ Movies.Infrastructure/Data/MovieContextSeed.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Logging;
+using Movies.Core.Entities;
+
+namespace Movies.Infrastructure.Data
+{
+    public class MovieContextSeed
+    {
+        public static async Task SeedAsync(MovieContext movieContext, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            var retryForAvailability = retry;
+            try
+            {
+                await movieContext.Database.EnsureCreatedAsync();
+                if (!movieContext.Movies.Any())
+                {
+                    movieContext.Movies.AddRange(GetPreconfiguredMovies());
+                    await movieContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (retryForAvailability < 3)
+                {
+                    retryForAvailability++;
+                    var logger = loggerFactory.CreateLogger<MovieContextSeed>();
+                    logger.LogError(ex, "An error occurred seeding the DB. Retrying... Attempt {RetryAttempt}", retryForAvailability);
+                    await SeedAsync(movieContext, loggerFactory, retryForAvailability);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        private static IEnumerable<Movie> GetPreconfiguredMovies()
+        {
+            return
+            [
+                new Movie
+                {
+                    MovieName = "The Shawshank Redemption",
+                    DirectorName = "Frank Darabont",
+                    ReleaseYear = "1994"
+                },
+                new Movie
+                {
+                    MovieName = "The Godfather",
+                    DirectorName = "Francis Ford Coppola",
+                    ReleaseYear = "1972"
+                },
+                new Movie
+                {
+                    MovieName = "The Dark Knight",
+                    DirectorName = "Christopher Nolan",
+                    ReleaseYear = "2008"
+                }
+            ];
+
+        }
+    }
+
+    
+}
