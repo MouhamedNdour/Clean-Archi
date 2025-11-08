c44f62eaece0421c4f426fc9a6ca947828971e0c
Mouhamed Aminata Madieng NDOUR <mouhamed.ndour@younited-credit.fr>
2025-11-08 18:10:59 +0100

    Introduce CQRS pattern and add movie-related features
    Removed unused Class1. Added CreateMovieCommand and
    GetMoviesByDirectorNameQuery with their respective handlers
    (CreateMovieCommanHandler and GetMoviesByDirectorNameHandler)
    to implement the CQRS pattern. Introduced AutoMapper with
    MovieMapper and MovieMappingProfile for entity-to-DTO mapping.
    Added MovieResponse DTO for movie operations. Updated
    Movies.Application.csproj to include AutoMapper, FluentValidation,
    and MediatR dependencies.

---
+++ Movies.Application/Handlers/CreateMovieCommanHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Movies.Application.Commands;
+using Movies.Application.Mappers;
+using Movies.Application.Responses;
+using Movies.Core.Entities;
+using Movies.Core.Repositories;
+
+namespace Movies.Application.Handlers
+{
+    public class CreateMovieCommanHandler : IRequestHandler<CreateMovieCommand, MovieResponse>
+    {
+        private readonly IMovieRepository movieRepository;
+
+        public CreateMovieCommanHandler(IMovieRepository movieRepository)
+        {
+            this.movieRepository = movieRepository;
+        }
+        public async Task<MovieResponse> Handle(CreateMovieCommand request, CancellationToken cancellationToken)
+        {
+            var movieEntity = MovieMapper.Mapper.Map<Movie>(request);
+            if(movieEntity is null)
+            {
+                throw new ArgumentNullException(nameof(movieEntity));
+            }
+
+           var newMovie = await movieRepository.AddAsync(movieEntity);
+            return MovieMapper.Mapper.Map<MovieResponse>(newMovie);
+        }
+    }
+}
---
+++ Movies.Application/Mappers/MovieMapper.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+
+
+
+namespace Movies.Application.Mappers
+{
+    public class MovieMapper
+    {
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = pi => pi.GetMethod.IsPublic || pi.SetMethod.IsPublic;
+                cfg.AddProfile<MovieMappingProfile>();
+            }, null);
+
+            var mapper = config.CreateMapper();
+            return mapper;
+        });
+
+        public static IMapper Mapper => lazy.Value;
+    }
+}
