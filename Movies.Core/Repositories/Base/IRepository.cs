13c0895efa770558ea9d0cbdb4553338aa657cee
Mouhamed Aminata Madieng NDOUR <mouhamed.ndour@younited-credit.fr>
2025-11-08 15:52:54 +0100

    Add project files.

---
+++ Movies.Core/Repositories/Base/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Movies.Core.Entities.Base;
+
+namespace Movies.Core.Repositories.Base
+{
+    public interface IRepository<T> where T : Entity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
