0615ae2144ccb58c45cffca13ee4c038c466335c
Mouhamed Aminata Madieng NDOUR <mouhamed.ndour@younited-credit.fr>
2025-11-08 15:57:47 +0100

    Add IMovieRepository interface with director filter method

---
+++ Movies.Core/Repositories/IMovieRepository.cs
@@ -0,0 +1,10 @@
+﻿using Movies.Core.Entities;
+using Movies.Core.Repositories.Base;
+
+namespace Movies.Core.Repositories
+{
+    public interface IMovieRepository : IRepository<Movie>
+    {
+        Task<IReadOnlyList<Movie>> GetMoviesByDirectorNameAsync(string directorName);
+    }
+}
